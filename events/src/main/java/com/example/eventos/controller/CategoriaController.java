49260145dd2e0d5e2d60b057ca40153ecbbf04de
Gabriel José Gomes <85190212+Gabrieljoseg@users.noreply.github.com>
2023-12-06 17:15:44 +0000

    changed-pom

--- events/src/main/java/com/example/eventos/controller/CategoriaController.java
+++ events/src/main/java/com/example/eventos/controller/CategoriaController.java
@@ -1,15 +1,15 @@
 package com.example.eventos.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.eventos.service.*;
-import com.example.eventos.model.*;
-
-import java.util.List;
+import com.example.eventos.model.Categoria;
+import com.example.eventos.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
